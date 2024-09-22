62dc749d8297b12b8727ee78671e6857de985e4a
Linas Vepštas <linasvepstas@gmail.com>
2024-09-22 17:23:03 -0500

    Labels can start with underscores

--- gas/config/tc-i370.c
+++ gas/config/tc-i370.c
@@ -1763,7 +1763,11 @@ symbol_locate (symbolS *symbolP,
    character is considered to be a label (since symbols
    can never be operands), and since we've already handled
    register operands. For example, "BL .L33" branch low
-   to .L33 RX form insn frequently terminates for-loops.  */
+   to .L33 RX form insn frequently terminates for-loops.
+
+   Local labels can start with underscores; this is for
+   MVS/HLASM compatibility.
+  */
 
 static bfd_boolean
 i370_addr_offset (expressionS *exx)
@@ -1779,7 +1783,7 @@ i370_addr_offset (expressionS *exx)
     {
       if (ISDIGIT (*lab))
 	all_digits = 1;
-      else if (ISALPHA (*lab))
+      else if (ISALPHA (*lab) || '_' == *lab)
 	{
 	  if (!all_digits)
 	    {
