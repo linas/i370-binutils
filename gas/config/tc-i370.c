56f98a55450814e3bbb1ba9b059d288f73edb9d2
Linas Vepštas <linasvepstas@gmail.com>
2024-10-04 18:05:23 -0500

    Bug-fix prior commit

--- gas/config/tc-i370.c
+++ gas/config/tc-i370.c
@@ -1325,6 +1325,10 @@ i370_dc_align (char type)
 
   switch (type)
     {
+    case 'C':  /* No alignment */
+      align2 = 0;
+      break;
+
     case 'H':  /* 16-bit decimal */
       align2 = 1;
       break;
