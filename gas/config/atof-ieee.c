75aae2902ee730e3c776d0685f1dd7842c0fefc4
Linas Vepštas <linasvepstas@gmail.com>
2024-11-03 23:21:51 -0600

    Try again w/ max float.

--- gas/config/atof-ieee.c
+++ gas/config/atof-ieee.c
@@ -370,7 +370,7 @@ gen_to_words (words, precision, exponent_bits)
 #else /* ! TC_I386  */
 #ifdef TC_I370
      /* This is converted to largest possible float. */
-	  words[0] = 0x7dff;
+	  words[0] = 0x7ebf;
 	  words[1] = 0xffff;
 	  words[2] = 0xffff;
 	  words[3] = 0xffff;
@@ -385,7 +385,7 @@ gen_to_words (words, precision, exponent_bits)
 	{
 #ifdef TC_I370
      /* This is converted to largest possible float. */
-	  words[0] = 0x7dff;
+	  words[0] = 0x7ebf;
 	  words[1] = 0xffff;
 	  words[2] = 0xffff;
 	  words[3] = 0xffff;
