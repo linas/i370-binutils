f6650051cc3e0ea75ddae733c6a8637655979285
Linas Vepštas <linasvepstas@gmail.com>
2024-11-16 00:19:19 -0600

    Add support for HLASM EXTRN

--- gas/config/tc-i370.c
+++ gas/config/tc-i370.c
@@ -892,7 +892,7 @@ char * i370_canonicalize_symbol_name (char *name)
   return name;
 }
 
-/* Provide minimal support for HLASM externals.
+/* Provide minimal support for HLASM externs and entry points.
 
    For example,
        ENTRY @@CRT0
@@ -904,6 +904,12 @@ char * i370_canonicalize_symbol_name (char *name)
 
    This is meant to be more or less the same as the elf `.globl __crt0`
    See the function s_globl() in read.c for a fancy example.
+
+   The EXTRN pseudo-op points here as well; see md_pseudo_table[] below.
+   The "conceptual" difference is that EXTRN declares a (global) symbol
+   that is resolved externally, whereas ENTRY declares a symbol that
+   is resolved in this very same file. From a practical standpoint,
+   there does not seem to be any difference in what we need to do.
 */
 
 static void
@@ -931,7 +937,6 @@ i370_entry (int unused ATTRIBUTE_UNUSED)
 
   dwarf2_emit_label(symbolP);
 }
-
 
 #define BIGNUM_CACHE 8    /* Eight LITTLENUM_TYPE's so 16 bytes total */
 
@@ -3094,6 +3099,7 @@ const pseudo_typeS md_pseudo_table[] =
   { "csect",    i370_csect,	0 },
   { "dsect",    i370_dsect,	0 },
   { "entry",    i370_entry,	0 },
+  { "extrn",    i370_entry,	0 },
 
   /* enable ebcdic strings e.g. for 3270 support */
   { "ebcdic",   i370_ebcdic,	1 },
